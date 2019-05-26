fc6499993226669c77b0810128273de74c2c49ac
Ayush Dubey <dubeyaayush07@gmail.com>
2019-05-26 19:40:33 +0530

    Updates

---
+++ precache-manifest.bb4498d7a66dbfeacb9d338aa9b586d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa092721d3eb29275c79f0529d33fecb",
+    "url": "/game-of-life/index.html"
+  },
+  {
+    "revision": "76b5af099a78ab03e7f0",
+    "url": "/game-of-life/static/css/main.860c25cb.chunk.css"
+  },
+  {
+    "revision": "1744db918d7799a6e6d6",
+    "url": "/game-of-life/static/js/2.164617b5.chunk.js"
+  },
+  {
+    "revision": "76b5af099a78ab03e7f0",
+    "url": "/game-of-life/static/js/main.f422ede3.chunk.js"
+  },
+  {
+    "revision": "b9d05e43578a5ddb80d1",
+    "url": "/game-of-life/static/js/runtime~main.dfaa7c2d.js"
+  }
+]);
